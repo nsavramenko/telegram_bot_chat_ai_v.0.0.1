190be0a2ad90d6e988b3c26b2aa5abab9a1a3ee5
nsavramenko <navramenko.work@gmail.com>
2023-09-17 13:24:32 -0200

    [FIRST] Initial commit

---
+++ src/main/java/com/example/telegram_bot_chat_ai/TelegramBotChatAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegram_bot_chat_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotChatAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotChatAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telegram_bot_chat_ai/TelegramBotChatAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegram_bot_chat_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotChatAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
